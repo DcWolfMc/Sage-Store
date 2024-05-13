8fb9ba076633de173eaee2e62730ef1cdf09a715
danielmcolares <danielmcolares@unifor.br>
2024-05-13 14:55:30 -0300

    footer done

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 import { ArrowUp } from "@phosphor-icons/react/dist/ssr";
+import Image from "next/image";
 import Link from "next/link";
 import { FunctionComponent } from "react";
 import useWindowDimensions from "@/utils/useWindowDimensions";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 export const Footer: FunctionComponent = () => {
-  const {width} = useWindowDimensions()
-  
+  const { width } = useWindowDimensions();
+
   const links = [
     { title: "Products", path: "/products" },
     { title: "About", path: "/about" },
@@ -23,7 +24,10 @@ export const Footer: FunctionComponent = () => {
           <div className="flex flex-col gap-1 w-fit">
             <strong>Navigation</strong>
             {
-              <Link href={"/login"} className="text-slate-500 text-xs hover:underline">
+              <Link
+                href={"/login"}
+                className="text-slate-500 text-xs hover:underline"
+              >
                 Enter
               </Link>
             }
@@ -37,27 +41,38 @@ export const Footer: FunctionComponent = () => {
               </Link>
             ))}
           </div>
-          { width>=768&&
+          {width >= 768 && (
             <span className="text-slate-500/50 dark:text-slate-500 truncate">
-            Copyright 2024 Daniel Colares
+              Copyright 2024 Daniel Colares
             </span>
-          }
+          )}
           <div className="flex flex-col gap-1 w-fit">
             <strong>Follow us</strong>
             <div className="flex flex-row gap-2 items-center">
-              <Link target="_blank" href={'https://github.com/DcWolfMc'}>
-              <FaGithub size={24} />
+              <Link target="_blank" href={"https://github.com/DcWolfMc"}>
+                <FaGithub size={24} />
               </Link>
-              <Link target="_blank" href={'https://www.linkedin.com/in/daniel-colares-b590681b6/'}>
-              <FaLinkedin size={24} />
+              <Link
+                target="_blank"
+                href={"https://www.linkedin.com/in/daniel-colares-b590681b6/"}
+              >
+                <FaLinkedin size={24} />
               </Link>
             </div>
           </div>
-          { width<768&&
+          {width < 768 && (
             <span className="text-slate-500/50 dark:text-slate-500 truncate w-fit">
-            Copyright 2024 Daniel Colares
+              Copyright 2024 Daniel Colares
             </span>
-          }
+          )}
+        </div>
+        <div className="flex flex-row justify-center items-center gap-2 py-2 border-t border-slate-500">
+        <div className="relative min-h-[40px] min-w-11">
+        <Image src={"/logo.png"} alt="logo image" fill />
+        </div>
+        <strong className="text-2xl xl:flex text-nowrap">
+        Sage Store
+        </strong>
         </div>
       </div>
     </footer>
