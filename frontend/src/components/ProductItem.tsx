22bbb85955cc45a9db1b188cd60cebc43643d4e6
danielmcolares <danielmcolares@unifor.br>
2024-05-22 15:28:57 -0300

    ProductItem for Products

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -33,24 +33,35 @@ export const ProductItem: FunctionComponent<ProductItemProps> = ({
     return price * ((100 - discount) / 100);
   }
   return (
-    <div className="border border-slate-500 rounded-lg">
-      <div className="relative max-w-[250px] max-h[300px]">
-        <Image className="object-contain" alt={title} src={thumbnail} fill />
+    <div className="max-w-[320px] w-full py-4 flex flex-col justify-center items-center gap-2 border border-slate-500 rounded-lg">
+      <div className="relative w-full h-[300px]">
+        <Image className="p-2 object-cover" alt={title} src={thumbnail} fill />
       </div>
-      <div>
-        <h3 className="text-lg line-clamp-2">{title}</h3>
+      <div className="flex-1 w-full px-8">
+        <h3 className="text-lg line-clamp-2 text-wrap">{title}</h3>
         <Tag tagName={brand} interactable={false} />
         <div className="flex flex-row gap-2 items-center">
-          <RatingStars rating={rating} color="#FCD34D"/>
+          <RatingStars
+            className="text-amber-500 dark:text-amber-400"
+            rating={rating}
+          />
           <span>{rating.toFixed(2)}</span>
         </div>
-        <strong className="text-2xl">USD$ {showPrice()}</strong>
+        <strong className="text-2xl">USD$ {showPrice().toFixed(2)}</strong>
         <div className="flex flex-row items-center gap-2 text-lg">
-          <strong className="text-slate-500 line-through">USD$ {showPrice()}</strong>
+          <strong className="text-slate-500 line-through">
+            USD$ {price.toFixed(2)}
+          </strong>
           <strong className="text-violet-700 ">{discount}%</strong>
         </div>
       </div>
-      {stock <= 50 && <div></div>}
+      {stock <= 50 ? (
+        <div className="w-full text-center font-bold bg-amber-500 text-slate-100 dark:text-slate-900">
+          Last Units
+        </div>
+      ) : (
+        <div className="h-6"></div>
+      )}
     </div>
   );
 };
