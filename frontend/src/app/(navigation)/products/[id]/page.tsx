323f4271a714d3de14f6bd44792aa6f00f983767
danielmcolares <danielmcolares@unifor.br>
2024-05-23 11:41:43 -0300

    using params

--- frontend/src/app/(navigation)/products/[id]/page.tsx
+++ frontend/src/app/(navigation)/products/[id]/page.tsx
@@ -1,3 +1,20 @@
-export default function ViewProduct() {
-  return <div>ProductID</div>;
+import {NextPage} from 'next/types'
+interface ProductPageParams {
+  params:{id: string};
+  // Adicione outras propriedades que você precisa passar para a página
 }
+
+const ProductPage:NextPage<ProductPageParams> = ({params}) => {
+
+  console.log("params:", params);
+  
+  
+  return (
+    <div>
+      <h1>Produto ID: {params.id}</h1>
+      {/* Renderize outras informações sobre o produto aqui */}
+    </div>
+  );
+};
+
+export default ProductPage;
