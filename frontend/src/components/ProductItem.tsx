0637dfdedbc5f5a37d770c4cd17793d00805be29
danielmcolares <danielmcolares@unifor.br>
2024-05-23 11:41:24 -0300

    navigation

--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { FunctionComponent } from "react";
 import { Tag } from "./Tag";
 import { RatingStars } from "./RatingStars";
+import Link from "next/link";
 
 interface ProductItemProps {
   id: number;
@@ -33,8 +34,11 @@ export const ProductItem: FunctionComponent<ProductItemProps> = ({
     return price * ((100 - discount) / 100);
   }
   return (
-    <div className="max-w-full md:max-w-[320px] w-full py-4 flex flex-col justify-center items-center gap-2 border border-slate-500 rounded-lg">
-      <div className="w-full flex flex-row md:flex-col">
+    <Link
+      href={`products/${id}`}
+      className="max-w-full md:max-w-[320px] w-full py-4 flex flex-col justify-center md:justify-between items-center gap-2 border border-slate-500 rounded-lg transition-all hover:bg-slate-500/30"
+    >
+      <div className="w-full flex flex-row md:flex-col ">
         <div className="relative w-[128px]  md:w-full h-[160px] md:h-[300px] flex items-center">
           <Image
             className="p-2 object-cover md:object-cover"
@@ -44,22 +48,30 @@ export const ProductItem: FunctionComponent<ProductItemProps> = ({
           />
         </div>
         <div className="flex-1 w-full px-4 md:px-8">
-          <h3 className="text-base md:text-lg line-clamp-2 text-wrap">{title}</h3>
+          <h3 className="text-base md:text-lg line-clamp-2 text-wrap">
+            {title}
+          </h3>
           <Tag tagName={brand} interactable={false} checked />
           <div className="flex flex-row gap-2 items-center align-middle">
             <RatingStars
               className=" text-amber-500 dark:text-amber-400"
               rating={rating}
             />
-            <span className="text-sm md:text-base align-middle">{rating.toFixed(2)}</span>
+            <span className="text-sm md:text-base align-middle">
+              {rating.toFixed(2)}
+            </span>
           </div>
-          <strong className="text-lg md:text-2xl">USD$ {showPrice().toFixed(2)}</strong>
-          <div className="flex flex-col md:flex-row items-start md:items-center gap-1 text-md md:text-lg">
-            <strong className="text-slate-500 line-through">
-              USD$ {price.toFixed(2)}
-            </strong>
-            <strong className="text-violet-700 ">{discount}%</strong>
-          </div>
+          <strong className="text-lg md:text-2xl">
+            USD$ {showPrice().toFixed(2)}
+          </strong>
+          {!discount|| discount!==0 && (
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-1 text-md md:text-lg">
+              <strong className="text-slate-500 line-through">
+                USD$ {price.toFixed(2)}
+              </strong>
+              <strong className="text-violet-700 ">{discount}%</strong>
+            </div>
+          )}
         </div>
       </div>
       {stock <= 50 ? (
@@ -69,6 +81,6 @@ export const ProductItem: FunctionComponent<ProductItemProps> = ({
       ) : (
         <div className="h-6"></div>
       )}
-    </div>
+    </Link>
   );
 };
